e6f8c06f85cecff5abf2082ba1c9f3839a2d2e78
Yukitake Simotsuki <icybamboon@outlook.com>
2023-02-03 09:25:47 +0800

    Code Upload

---
+++ AyaBlackScreen/Form1.Designer.cs
@@ -0,0 +1,54 @@
+﻿namespace AyaBlackScreen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(537, 363);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "AYANEO Air 黑屏程序";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ AyaBlackScreen/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AyaBlackScreen
+{
+    public partial class Form1 : Form
+    {
+        int clickTimes;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll", EntryPoint = "ShowCursor", CharSet = CharSet.Auto)]
+        public static extern void ShowCursor(int status);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            clickTimes = 0;
+            ShowCursor(0);
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            clickTimes++;
+            if(clickTimes >= 3)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
